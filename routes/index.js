87e2456b9f6fec5e7603471deac70b26fa9ba851
CannonFodderr <izhaki.idan@gmail.com>
2018-04-25 11:55:02 +0300

    Fix store selector CSS

--- routes/index.js
+++ routes/index.js
@@ -17,9 +17,11 @@ formIsFilled = (req, res, next) => {
 }
 
 router.get('/', (req, res) => res.redirect('/register'));
+// Display form page
 router.get('/register', (req, res) => {
     res.render('index');
 });
+// Post a new form
 router.post('/register',formIsFilled, (req, res) => {
     let sanitizedName = req.sanitize(req.body.user.fullName);
     let newUser = {
@@ -43,10 +45,12 @@ router.post('/register',formIsFilled, (req, res) => {
             User.create(newUser, (err, createdUser) => {
                 if(err){ 
                     console.log(err);
+                    alert("נתקלנו בבעיה אנא מלא את הטופס שנית");
                     res.redirect('/');
                 } else {
                     createdUser.products.push(newProduct);
                     createdUser.save();
+                    alert("ההרשמה בוצעה בהצלחה!");
                     res.redirect('/register');
                 }
             })
@@ -54,6 +58,7 @@ router.post('/register',formIsFilled, (req, res) => {
             console.log('Found Email adding to products array');
             foundEmail.products.push(newProduct);
             foundEmail.save();
+            alert("ההרשמה נוספה בהצלחה!");
             res.redirect('/register');
         }
     });
